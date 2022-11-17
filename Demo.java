4c6d3c959b311aa0ce2cb83dfe281f06e2844873
ayushisushma <118463901+ayushisushma@users.noreply.github.com>
2022-11-17 21:59:31 +0530

    done by sushma

---
+++ Demo.java
@@ -0,0 +1,23 @@
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("_________________marksheet of univercity________________");
+		System.out.println("________________________________________________________");
+		System.out.println("name:-cheatan      roll_no:-1      semater:-4");
+		System.out.println("________________________________________________________");
+		System.out.println("Subject         ||     Min Mark   ||     Obtain mark    ");
+		System.out.println("ADS             ||        40      ||          50        ");
+		System.out.println("DM              ||        40      ||          60        ");
+		System.out.println("TOC             ||        40      ||          95        ");
+		System.out.println("CE              ||        40      ||          75        ");
+		System.out.println("CAO             ||        40      ||          60        ");
+		System.out.println("________________________________________________________");
+		System.out.println("TOTAL           ||        200     ||          349       ");
+		System.out.println("________________________________________________________");
+		System.out.println("Average         ||                ||          60        ");
+		System.out.println("________________________________________________________");
+		System.out.println("RESULT");
+	}
+
+}
